34a7aa429aff6bb381aaed95e8edf1e31327c8d5
MUNOZ Joseph <153305690+mateoMUNOZlml@users.noreply.github.com>
2024-11-25 13:25:08 +0000

    herculex .h .c

--- tp4_pwm/Core/Inc/herculex.h
+++ tp4_pwm/Core/Inc/herculex.h
@@ -7,9 +7,13 @@
 
 #ifndef INC_HERCULEX_H_
 #define INC_HERCULEX_H_
+#include <stdint.h>
+#include "main.h"
+#include "usart.h"
 
-uint8_t calcul_checksum1(uint8_t size,uint8_t trame);
+
+uint8_t calcul_checksum1(uint8_t size, uint8_t *trame);
 uint8_t calcul_checksum2(uint8_t checksum1);
-void send_trame(uint8_t id,uint8_t size, uint8_t trame);
+void send_trame(uint8_t id,uint8_t size, uint8_t *trame);
 
 #endif /* INC_HERCULEX_H_ */
