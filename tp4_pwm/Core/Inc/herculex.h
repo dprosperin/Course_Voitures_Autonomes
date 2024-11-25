ab027b005c3fd6c671b464163dead9b923160d5d
MUNOZ Joseph <153305690+mateoMUNOZlml@users.noreply.github.com>
2024-11-25 12:46:17 +0000

    matheo
    matheo

--- tp4_pwm/Core/Inc/herculex.h
+++ tp4_pwm/Core/Inc/herculex.h
@@ -8,8 +8,8 @@
 #ifndef HERCULEX_H_
 #define HERCULEX_H_
 
-
 uint8_t calcul_checksum1(uint8_t size,uint8_t trame);
 uint8_t calcul_checksum2(uint8_t checksum1);
 void send_trame(uint8_t id,uint8_t size, uint8_t trame);
+matheo ;
 #endif /* HERCULEX_H_ */
