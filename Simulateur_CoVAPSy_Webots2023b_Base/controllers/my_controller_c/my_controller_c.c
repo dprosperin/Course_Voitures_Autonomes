41c306636a65f8e81d9456a6494e91c96b196e9b
ymesbahige1 <yassine.mesbahi@universite-paris-saclay.fr>
2024-12-02 14:30:54 +0100

    test non concluant

--- Simulateur_CoVAPSy_Webots2023b_Base/controllers/my_controller_c/my_controller_c.c
+++ Simulateur_CoVAPSy_Webots2023b_Base/controllers/my_controller_c/my_controller_c.c
@@ -31,7 +31,7 @@ unsigned char modeAuto=0;
 float max_90 , max_270,max = 0.0 ; 
 float angle_calculé = 0.0 ; 
 float maxangle = 0.35 ; 
-float MAX= 0.0 ; 
+float MAX= 0.35 ; 
 // prototype des fonctions
 void affichage_consigne();
 void set_direction_degres(float angle_degre);
@@ -39,7 +39,6 @@ void set_vitesse_m_s(float vitesse_m_s);
 unsigned char gestion_appui_clavier(void);
 void recule(void);
 void set_angle_test() ; 
- 
 //vitesse en km/h
 float speed = 0;
 float maxSpeed = 28; //km/h
@@ -54,6 +53,7 @@ float maxangle_degre = 16;
   
 int main(int argc, char **argv) 
 {
+ prtinf ("a\n") ; 
   unsigned int i;
   signed int data_lidar_mm_main[360];
   float angle_degre, vitesse_m_s;
@@ -73,6 +73,7 @@ int main(int argc, char **argv)
   set_vitesse_m_s(0);
   while (wbu_driver_step() != -1) 
   {
+   printf("b\n");
     float distance;
     /* lire le lidar et traiter les données :   */
     range_donnees=wb_lidar_get_range_image(lidar);
@@ -98,10 +99,11 @@ int main(int argc, char **argv)
         /*  - la fonction set_vitesse_m_s(...)  */
         /*  - la fonction recule()              */
         /****************************************/
-        //angle_degre = 0.02*(data_lidar_mm_main[60]-data_lidar_mm_main[300]); //distance à 60° - distance à -60°
+       // angle_degre = 0.02*(data_lidar_mm_main[60]-data_lidar_mm_main[300]); //distance à 60° - distance à -60°
         //set_direction_degres(angle_degre);
         vitesse_m_s = 0.5;
         set_vitesse_m_s(vitesse_m_s);
+         printf("c");  
     }
   }
   /* This is necessary to cleanup webots resources */
@@ -133,6 +135,7 @@ unsigned char gestion_appui_clavier(void)
       {
         modeAuto = 1;
         printf("------------Mode Auto Activé-----------------");
+         printf("c\n");
       }
       break;
       
@@ -172,32 +175,35 @@ void set_vitesse_m_s(float vitesse_m_s){
 void recule(void){
     wbu_driver_set_cruising_speed(-1);
 }
-void set_angle_test() 
+
+void set_angle_test(signed int data_lidar_mm_main[])
 {
-max = 0 ; 
-max_270 = 0 ; 
-max_90 = 0 ; 
-for (int i = 0 ; i<90 ; i++) 
-{
-if (data_lidar_mm_main[i]>max_90)
-{max_90 = data_lidar_mm_main[i];}
-else {max_90 = max_90;}
+    max = 0; 
+    max_270 = 0; 
+    max_90 = 0; 
 
+    for (int i = 0; i < 90; i++) 
+    {
+        if (data_lidar_mm_main[i] > max_90)
+            max_90 = data_lidar_mm_main[i];
+    }
+    for (int i = 270; i < 360; i++) 
+    {
+        if (data_lidar_mm_main[i] > max_270)
+            max_270 = data_lidar_mm_main[i];
+    }
 
-}
-for (int i = 270 ; i<360 ; i++) 
-{
-if (data_lidar_mm_main[i]>max_270)
-{max_270 = data_lidar_mm_main;}
-else {max_270 = max_270;}
-}
-if (max_270>max_90)
-{max = max_270;}
-else {max=max_90;}
+    if (max_270 > max_90)
+        max = max_270;
+    else
+        max = max_90;
 
-angle_calculé = (max_90 * MAX) / max_270;
+    if (max_270 != 0)
+        angle_calculé = (max_90 * MAX) / max_270;
+    else
+       angle_calculé = 0;
 
-  wbu_driver_set_steering_angle(angle_calculé);
-
+    wbu_driver_set_steering_angle(angle_calculé);
+ printf("d\n");
 }
 
