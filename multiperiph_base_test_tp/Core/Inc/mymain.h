e24a6ced0321af88071f4da980901b9c5877c7e5
David Prospérin <32713756+dprosperin@users.noreply.github.com>
2025-01-08 20:02:17 +0100

    Ajout fonctions de déplacements

--- multiperiph_base_test_tp/Core/Inc/mymain.h
+++ multiperiph_base_test_tp/Core/Inc/mymain.h
@@ -8,10 +8,19 @@
 #ifndef INC_MYMAIN_H_
 #define INC_MYMAIN_H_
 
+#include <stdbool.h>
+
 void setup();
 
 void loop();
 
-uint16_t set_degre();
+void set_angle_test(void);
+void set_vitesse_et_sens(float nouvelle_vitesse, bool nouveau_sens);
+void set_angle(float nouvelle_angle);
+void recule(void);
+void avance(void);
+
+#define CAN_ID_MOTEUR 0x700
+#define CAN_ID_HERKULEX 0x701
 
 #endif /* INC_MYMAIN_H_ */
--- multiperiph_base_test_tp/Core/Src/mymain.c
+++ multiperiph_base_test_tp/Core/Src/mymain.c
@@ -5,34 +5,15 @@
  *      Author: yassine.mesbahi
  */
 #include "fdcan.h"
-#include "stm32g4xx_hal.h"
+#include "mymain.h"
+#include <stm32g4xx_hal.h>
+#include <stdbool.h>
 
 FDCAN_TxHeaderTypeDef header;
-uint8_t data[3];
 
-uint8_t vitesse_m_s = 0 ;
-uint8_t bit_fort ;
-uint8_t bit_faible ;
-uint16_t set_angle()
-{
-	 float data_lidar_main[360];
-
-	 data_lidar_main [345] = 1405.0; // Assign float value
-	 data_lidar_main[45] = 1404.0;  // Assign another float value
-
-	 float difference  = data_lidar_main[345]-data_lidar_main [45] ;
-	 float kp = 0.60 ;
-	 float angle = 0.0 ;
-
-	 uint16_t hexa_angle ;
-
-	 angle = kp * difference ;
-
-	 hexa_angle = 0x200+(angle/0.35) ;
-
-
-	 return hexa_angle ;
-}
+float vitesse = 0.0;
+bool  sens = 0;
+float angle = 0.0;
 
 void setup()
 {
@@ -54,29 +35,111 @@ void setup()
 
 void loop()
 {
-	//if ( 0xFFFFFFFFFFFFFF9C < set_angle() && set_angle() < 0x64 )
-	//{
-		vitesse_m_s = 10;
-	//}
-	//else
-//	{
-		//vitesse_m_s=5;
-	//}
 
-	bit_faible = set_angle() & 0xFF ;
-	bit_fort = (set_angle()>>8)&0xFF;
-
-
-
-
-
-
-	data [1] = bit_faible ;
-	data [0] = 	bit_fort ;
-	data [2] = vitesse_m_s ;
-
-
-	HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &header, data) ;
+	set_angle_test();
     HAL_Delay(1000);
+}
+
+////  Fonctions de navigation
+void set_angle_test(void)
+{
+	 float data_lidar_main[360];
+
+	 data_lidar_main [345] = 1405.0; // Assign float value
+	 data_lidar_main[45] = 1404.0;  // Assign another float value
+
+	 float difference  = data_lidar_main[345]-data_lidar_main [45] ;
+	 float kp = 0.60 ;
+	 float angle = 0.0 ;
+
+	 angle = kp * difference ;
+
+	set_angle(angle);
+}
+
+////  Fonctions de déplacements
+
+/**
+ * @brief
+ * @param  nouvelle_vitesse : un floatant compris entre 0 et 1
+ * @param  nouveau_sens : sens de rotation des roues
+ * @retval None
+ */
+void set_vitesse_et_sens(float nouvelle_vitesse, bool nouveau_sens)
+{
+	uint8_t octet_nouvelle_vitesse;
+	uint8_t octet_nouveau_sens;
+	uint8_t txData[2];
+
+	// On gère les cas limites
+	if (nouvelle_vitesse > 1)
+		vitesse = 1;
+	else if (nouvelle_vitesse < 0)
+		vitesse = 0;
+	else
+		vitesse = nouvelle_vitesse;
+
+	octet_nouvelle_vitesse = (uint8_t) (vitesse * 100);
+	octet_nouveau_sens     =  nouveau_sens;
+
+	// Premier octet la vitesse * 100
+	txData[0] = octet_nouvelle_vitesse;
+
+	// Second octet le sens de rotation
+	txData[1] = octet_nouveau_sens;
+
+	header.Identifier = CAN_ID_MOTEUR; // Set your CAN identifier
+	header.IdType = FDCAN_STANDARD_ID; // Standard ID
+	header.TxFrameType = FDCAN_DATA_FRAME; // Data frame
+	header.DataLength = 2; // Data length
+
+	HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &header, txData);
+
+	// TODO : Mettre une tempo
+}
+
+/**
+ * @brief  Fait reculer à la vitesse définit en global
+ * @retval None
+ */
+void recule(void)
+{
+	set_vitesse_et_sens(vitesse, 0);
+}
+
+/**
+ * @brief Fait avancer à la vitesse définit en global
+ * @retval None
+ */
+void avance(void)
+{
+	set_vitesse_et_sens(vitesse, 1);
+}
+
+void set_angle(float nouvelle_angle)
+{
+	uint8_t octet_fort_angle;
+	uint8_t octet_faible_angle;
+	uint8_t txData[2];
+	uint16_t cmd_angle;
+
+	//TODO : Fixer les limites de rotation du robot
+
+	header.Identifier = CAN_ID_HERKULEX; // Set your CAN identifier
+	header.IdType = FDCAN_STANDARD_ID; // Standard ID
+	header.TxFrameType = FDCAN_DATA_FRAME; // Data frame
+	header.DataLength = 2; // Data length
+
+	cmd_angle          = 0x200+(angle/0.35);
+
+	octet_faible_angle = (uint8_t) cmd_angle;
+	octet_fort_angle   = (cmd_angle >> 8) & 0x00FF;
+
+	txData[0] = octet_fort_angle;
+	txData[1] = octet_faible_angle;
+
+	HAL_FDCAN_AddMessageToTxFifoQ(&hfdcan1, &header, txData);
+
+	// TODO : Mettre une tempo
 }
 
